d9eefedb68ee5805b5f5649ee818e82098398496
Maxwell Flynn <me@maxwellflynn.com>
2019-10-13 00:26:31 -0400

    actually fixed conflict

--- src/index.js
+++ src/index.js
@@ -96,11 +96,8 @@ async function getPlaying(message) {
             return;
         }
 
-<<<<<<< HEAD
         db.close();
 
-=======
->>>>>>> 7a0e7efbb4f2aa134ba5a191c75f89b9b0a56976
         // set the options for getting the last.fm playing
         lastFMAPIOptions.qs.method = 'user.getrecenttracks';
         lastFMAPIOptions.qs.user   = user.lastFMUsername;
